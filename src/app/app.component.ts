4a671319ae3222e71a6048452d92a3822db3a44b
joellop <joel_blg@outlook.com>
2025-04-29 19:28:51 -0600

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { WelcomeComponent } from './sections/welcome/welcome.component';
+import { DetailsComponent } from './sections/details/details.component';
+import { StoryComponent } from './sections/story/story.component';
+import { RsvpComponent } from './sections/rsvp/rsvp.component';
+import { FooterComponent } from './sections/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [WelcomeComponent, DetailsComponent, StoryComponent, RsvpComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
